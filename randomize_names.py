4f17710c58e5830430153772ab216dc1b7712249
Denis Mitin <gravtit@yandex.ru>
2018-07-18 12:50:57 +0300

    Upload program (0.1)
    Program 0.1

---
+++ randomize_names.py
@@ -0,0 +1,23 @@
+import random, os
+alpha = list ("qwertyuiop[]asdfghjkl;'zxcvbnm,.1234567890QWERTYUIOP[]ASDFGHJKLZXCVBNM!@#$%^&()_+-=")
+cycle = random.randint(6,50)
+i=0
+name = ''
+while i < cycle:
+        get_name = random.randint(0,len(alpha)-1)
+        name += alpha[get_name]
+        i += 1
+old_folder = os.getcwd() + name
+if old_folder == True:
+        print ('There is same folder, start creating new name...')
+        while i < cycle:
+                name =''
+                get_name = random.randint(1,len(alpha)-1)
+                name += alpha[get_name]
+                i += 1
+else:
+        os.mkdir(name)
+print ('Rdy')
+
+
+
